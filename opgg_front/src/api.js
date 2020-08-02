432e2b0ffeb7be906de0e77ef751865618b8ad6d
YoungMin Lee <miniyoung37@gmail.com>
2020-08-03 01:36:16 +0900

    add api component

---
+++ opgg_front/src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://codingtest.op.gg/api/summoner",
+});
+
+export const apiList = {
+  getPlayer: (playName) => api.get(`/${playName}`),
+  getPlayerMost: (playName) => api.get(`/${playName}/mostInfo`),
+  getPlayerMatchList: (playName) => api.get(`/${playName}/matches`),
+  getPlayerMatchDetail: (playName, gameId) =>
+    api.get(`/${playName}/matchDetail/${gameId}`),
+  getItem: () =>
+    axios.get(
+      "http://ddragon.leagueoflegends.com/cdn/10.15.1/data/ko_KR/item.json"
+    ),
+};
